7b9daf88d2d4a1e54eeb8e224df08d3a6ae85693
sho.tanaka <sho.tanaka@aijus.com>
2019-12-01 10:23:22 +0900

    finished the part2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Quiz from './components/Quiz/Quiz';
 
 function App() {
+  // <ul>から</ul>は後で削除する
   return (
-    <div className="App">
-        Hello
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/Quiz/'>Quiz</Link>
+        </li>
+      </ul>
+      <Route path='/' exact component={Home} />
+      <Route path='/Quiz/' exact component={Quiz} />
+    </Router >
   );
 }
 
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Quiz = () => {
+  return <h1>クイズ</h1>;
+};
+
+export default Quiz;
