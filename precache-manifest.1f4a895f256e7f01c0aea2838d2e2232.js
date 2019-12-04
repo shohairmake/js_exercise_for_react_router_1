c89e06b53031935fac5197913677de049efc17b6
sho.tanaka <sho.tanaka@aijus.com>
2019-12-04 17:46:24 +0900

    Updates

---
+++ precache-manifest.1f4a895f256e7f01c0aea2838d2e2232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997f90566078daf4b11c53d31e754e7d",
+    "url": "/react_quiz_app/index.html"
+  },
+  {
+    "revision": "d8e5392b3dcab80552e5",
+    "url": "/react_quiz_app/static/css/main.b331cf76.chunk.css"
+  },
+  {
+    "revision": "3418701be6a489748ced",
+    "url": "/react_quiz_app/static/js/2.3d7cbb2a.chunk.js"
+  },
+  {
+    "revision": "d8e5392b3dcab80552e5",
+    "url": "/react_quiz_app/static/js/main.aa2e1ff3.chunk.js"
+  },
+  {
+    "revision": "547ae815beca62a99f0e",
+    "url": "/react_quiz_app/static/js/runtime-main.c2afffc2.js"
+  }
+]);
